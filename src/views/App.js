a1b41d154cf3f350d947c75a97f3cfc2bc3d3436
raulito <raul_0.4_@hotmail.com>
2021-10-19 11:12:10 +0200

    refactor in opinion component

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import OpinionContent from '../components/OpinionContent/OpinionContent.js';
+import Opinions from './Opinions';
 
 function App() {
 
@@ -7,19 +7,19 @@ function App() {
     {
       title: 'Wizard of pop-corns',
       comment: 'good, but more information is needed',
-      date: new Date(2020, 7 , 7),
+      date: new Date(2020, 7, 7),
       note: 5
     },
     {
       title: 'Wizard of chocolate',
       comment: 'boooring!',
-      date: new Date(1997, 6 , 7),
+      date: new Date(1997, 6, 7),
       note: 2
     },
     {
       title: 'Wizard of ice-cream',
       comment: 'good taste and amazing!',
-      date: new Date(2005, 9 , 8),
+      date: new Date(2005, 9, 8),
       note: 9
     }
   ]
@@ -32,25 +32,7 @@ function App() {
         </p>
         you can see below for our opinions
       </header>
-      <OpinionContent
-        title={peopleOpinions[0].title}
-        comment={peopleOpinions[0].comment}
-        note={peopleOpinions[0].note}
-        date={peopleOpinions[0].date}
-      ></OpinionContent>
-      <OpinionContent
-        title={peopleOpinions[1].title}
-        comment={peopleOpinions[1].comment}
-        note={peopleOpinions[1].note}
-        date={peopleOpinions[1].date}
-      ></OpinionContent>
-      <OpinionContent
-        title={peopleOpinions[2].title}
-        comment={peopleOpinions[2].comment}
-        note={peopleOpinions[2].note}
-        date={peopleOpinions[2].date}
-      ></OpinionContent>
-
+      <Opinions opinions={peopleOpinions} />
     </div>
   );
 }
---
+++ src/views/Opinions.js
@@ -0,0 +1,30 @@
+import './Opinions.css';
+import OpinionContent from '../components/OpinionContent/OpinionContent.js';
+
+function Opinions(props) {
+    return (
+        <div className="Opinions">
+            <OpinionContent
+                title={props.opinions[0].title}
+                comment={props.opinions[0].comment}
+                note={props.opinions[0].note}
+                date={props.opinions[0].date}
+            ></OpinionContent>
+            <OpinionContent
+                title={props.opinions[1].title}
+                comment={props.opinions[1].comment}
+                note={props.opinions[1].note}
+                date={props.opinions[1].date}
+            ></OpinionContent>
+            <OpinionContent
+                title={props.opinions[2].title}
+                comment={props.opinions[2].comment}
+                note={props.opinions[2].note}
+                date={props.opinions[2].date}
+            ></OpinionContent>
+
+        </div>
+    );
+}
+
+export default Opinions;
